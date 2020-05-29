12aaaf83494038884b4c369885938887b746bfd9
AdarshaSomanna <adarsha.somanna@astarsolution.com>
2020-05-29 17:04:57 +0530

    Updates

---
+++ precache-manifest.6a6f290f1bcf61e70ee6a0ce072483f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a1872390c404c887f4b5fd45b643b0",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "79e1ac19f58ba218612a",
+    "url": "/react-movie-app/static/css/main.f2b81235.chunk.css"
+  },
+  {
+    "revision": "6630f3e5974643e5b33f",
+    "url": "/react-movie-app/static/js/2.3132dd44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-app/static/js/2.3132dd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e1ac19f58ba218612a",
+    "url": "/react-movie-app/static/js/main.3b935351.chunk.js"
+  },
+  {
+    "revision": "83d4cf6b031c93e40a5b",
+    "url": "/react-movie-app/static/js/runtime-main.2013fc7c.js"
+  }
+]);
